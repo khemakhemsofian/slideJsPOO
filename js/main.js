ac8ec143beef0c491609b7f7b3c7bf3d9290bb3b
khemakhemsofian <sofian.khemakhem@gmail.com>
2021-10-20 16:41:21 +0200

    commit1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import {mySlider} from "./slider.js";
+import {mySlider} from "./module/slider.js";
 
 mySlider.clickSlider();
--- js/module/slider.js
+++ js/module/slider.js
@@ -2,15 +2,14 @@ var mySlider = {
  
  
  clickSlider : () =>{
-     alert("test");
+    
  const items = document.querySelectorAll('img');
  const nbSlide = items.length;
- const suivant = document.getElementById('precedent');
- const precedent = document.getElementById('suivant');
+ var suivant = document.getElementById('suivant');
+ var precedent = document.getElementById('precedent');
   let count = 0;
-
-   function slideSuivante() {
-     alert("test");
+   /* function slideSuivante() {
+     
        items[count].classList.remove('myImage');
 
        if (count < nbSlide -1) {
@@ -20,8 +19,32 @@ var mySlider = {
        }
        items[count].classList.add('myImage');
    }
-   suivant.addEvenListener('click', slideSuivante());
-   function slidePrecedente() {
+   suivant.addEvenListener('click', slideSuivante());*/
+   document.getElementById('suivant').addEventListener('click' , function () {
+    items[count].classList.remove('myImage');
+
+    if (count < nbSlide -1) {
+        count++;
+    } else{
+        count = 0;
+    }
+    items[count].classList.add('myImage');
+   });
+
+   document.getElementById('precedent').addEventListener('click' , function () {
+    items[count].classList.remove('myImage');
+    if (count > 0) {
+       count--;
+   } else{
+       count = nbSlide - 1;
+   }
+   items[count].classList.add('myImage');
+   });
+
+
+   
+   
+   /*function slidePrecedente() {
      items[count].classList.remove('myImage');
      if (count > 0) {
         count--;
@@ -30,13 +53,26 @@ var mySlider = {
     }
     items[count].classList.add('myImage');
    }
-   precedent.addEvenListener('click', slidePrecedente());
+   precedent.addEvenListener('click', slidePrecedente()); */
 
    function keyPress(e) {
        if (e.keyCode === 37) {
-           slidePrecedente();
+        items[count].classList.remove('myImage');
+        if (count > 0) {
+           count--;
+       } else{
+           count = nbSlide - 1;
+       }
+       items[count].classList.add('myImage');
        } else if (e.keyCode === 39) {
-           slideSuivante();
+        items[count].classList.remove('myImage');
+
+        if (count < nbSlide -1) {
+            count++;
+        } else{
+            count = 0;
+        }
+        items[count].classList.add('myImage');
        }
    }
    document.addEventListener('keydown', keyPress);
